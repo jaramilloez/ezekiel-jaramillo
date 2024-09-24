464bb352053a80f4eaf442c9f3336b32d9ac0614
Ezekiel Jaramillo <jaramilloez2004@gmail.com>
2024-09-23 22:13:29 -0600

    Add contact section

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="section">
+      <div className="header">Get In Contact</div>
+      <div className="text-center">
+        <div className="fs-3 pt-3">Lets Talk!</div>
+        <div className="contactLine">
+          Email •{" "}
+          <a href="mailto:jaramilloez2004@gmail.com">
+            jaramilloez2004@gmail.com
+          </a>
+        </div>
+        <div className="contactLine">
+          Phone • <a href="tel:3853686809">380-368-6809</a>
+        </div>
+        <div className="contactLine">
+          Social •{" "}
+          <a href="www.linkedin.com/in/ezekiel-jaramillo-31a742233">LinkedIn</a>{" "}
+          <strong>OR</strong>{" "}
+          <a href="https://github.com/jaramilloez">GitHub</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
